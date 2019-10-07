d64ae3550a2732a36986a9de6fd7b030ba59f9c9
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-10-07 16:52:03 +0900

    fix tlaks

--- components/talks.php
+++ components/talks.php
@@ -1,7 +1,33 @@
-<?php
-    $all_talk = file_get_contents('data/'.$_REQUEST['id'].'.json');
+<style>
+    .balloon-left {
+        position: relative;
+        display: inline-block;
+        margin: 1.5em 0 1.5em 15px;
+        padding: 7px 10px;
+        min-width: 120px;
+        max-width: 100%;
+        color: #555;
+        font-size: 16px;
+        background: #83F0A6;
+        border-radius: 10px;
+    }
+
+    .balloon-left p {
+        margin: 0;
+        padding: 0;
+    }
+</style>
+
+<div class="row">
+    <?php
+    $all_talk = file_get_contents('data/' . $_REQUEST['id'] . '.json');
     $array = json_decode($all_talk);
     foreach ($array as $key => $value) {
-        echo "<p>$value->Content</p>";
+        echo "
+            <div class=\"col-12 mt-0 mb-0\">
+                <p class=\"balloon-left w-100\">$value->Content</p>
+            </div>        
+        ";
     }
-?>
+    ?>
+</div>
