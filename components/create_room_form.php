7747bfc7a9642c0dcdb34818a39358c54ee3b4ec
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-10-07 16:17:15 +0900

    fix layouts

--- components/create_room_form.php
+++ components/create_room_form.php
@@ -1,7 +1,7 @@
 <form method="post" action="php/create_room.php">
     <div class="form-group">
-        <label for="newChatRoom">新規チャットルーム名</label>
+        <label for="newChatRoom" class="h2">新しいチャットルームを作成</label>
         <input class="form-control" id="newChatRoom" name="room" placeholder="新しく作るチャットルーム名を入力してください">
     </div>
-    <button type="submit" class="btn btn-primary">作成</button>
+    <button type="submit" class="btn btn-outline-danger">作成</button>
 </form>
