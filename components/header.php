28f4a6f6968b7e2dfd57f3025fb9decb62caca47
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-10-07 14:19:14 +0900

    add header component

---
+++ components/header.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-light bg-light">
+        <img src="https://getbootstrap.jp/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+        Samurai Chat!
+    </a>
+</nav>
--- index.php
+++ index.php
@@ -6,6 +6,6 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </head>
     <body>
-        <h1>Hello Chat's</h1>
+        <?php include('components/header.php'); ?>
     </body>
 </html>
