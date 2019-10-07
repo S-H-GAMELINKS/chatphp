ef0968d8a8de27bee32dfd18a95142f876b23987
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-10-07 16:01:34 +0900

    fix header color

--- components/header.php
+++ components/header.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-light bg-light" style="background-color: #99cc00;">
+<nav class="navbar navbar-dark" style="background-color:#F04242;">
     <a class="navbar-brand" href="/">
         <img src="https://getbootstrap.jp/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
         Samurai Chat!
