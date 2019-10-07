fcf7738c76c895566760a00ba73d5906fdd2f364
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-10-07 16:24:08 +0900

    fix layouts rooms

--- components/rooms.php
+++ components/rooms.php
@@ -2,6 +2,17 @@
     $all_room = file_get_contents('data/room.json');
     $array = json_decode($all_room);
     foreach ($array as $key => $value) {
-        echo "<a href=\"room.php?id=$key&name=$value->Content\" name=\"id\" value=\"$key\">No:$key $value->Content</a>";
+        echo "
+            <div class=\"row\">
+                <div class=\"col-md-4 col-sm-12\">
+                    <div class=\"card border border-danger\" style=\"width: 18rem;\">
+                        <div class=\"card-body\">
+                            <h5 class=\"card-title\">$value->Content</h5>
+                            <a href=\"room.php?id=$key&name=$value->Content\" name=\"id\" value=\"$key\">入室する</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        ";
     }
 ?>
